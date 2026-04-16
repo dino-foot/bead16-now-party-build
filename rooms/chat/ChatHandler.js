33013e16ea4b8aaf60d70f21f6c29da4bf89b8bb
Shohanur Rahaman <shohan4556@gmail.com>
2026-04-16 12:16:16 +0600

    Update ChatHandler.js

--- rooms/chat/ChatHandler.js
+++ rooms/chat/ChatHandler.js
@@ -1,27 +1,31 @@
+import { randomUUID } from "crypto";
 export class ChatHandler {
     constructor(room) {
         this.chatHistory = [];
         this.MAX_HISTORY = 25;
+        this.MAX_CONTENT_LENGTH = 50; //? can be moved to config
         this.room = room;
+        this.chatHistory = [];
     }
     // Call this inside onCreate
     setup() {
         this.room.onMessage("SEND_CHAT", (client, message) => {
             const player = this.room.state.players.get(client.sessionId);
             const chatData = {
-                senderId: client.sessionId,
-                senderName: player?.name || `Guest_${client.sessionId.substring(0, 4)}`,
-                avatarUrl: player?.avatarUrl || "0",
-                content: message.content ?? "hello!", // default content if missing
+                messageId: message.messageId ?? randomUUID(),
+                senderId: message.senderId ?? client.sessionId,
+                senderName: message?.senderName || `Guest_${client.sessionId.substring(0, 4)}`,
+                avatarUrl: message.avatarUrl ?? "0", // default avatar
+                content: message.content.length > this.MAX_CONTENT_LENGTH ? message.content.substring(0, this.MAX_CONTENT_LENGTH) : message.content || "Hi !", // default content if missing
                 type: message.type,
-                isPlayer: player ? !player.isSpectator : false
             };
             // Only save text history
-            if (chatData.type === "text") {
+            if (chatData.type === "TEXT") {
                 this.chatHistory.push(chatData);
                 if (this.chatHistory.length > this.MAX_HISTORY)
                     this.chatHistory.shift();
             }
+            console.log(`[ON SEND_CHAT] chat message :`, message.content);
             // Broadcast to everyone so they see the text in the ui immediately
             this.room.broadcast("RECEIVE_CHAT", chatData);
         });
@@ -31,5 +35,27 @@ export class ChatHandler {
         if (this.chatHistory.length > 0) {
             client.send("CHAT_HISTORY", this.chatHistory);
         }
+    }
+    //? for debug purpose
+    addChatMessage() {
+        // const chatData: ChatMessage = {
+        //     messageId: randomUUID(),
+        //     senderId: `seesionId_${randomUUID()}`,
+        //     senderName: `Server_${randomUUID()}`,
+        //     avatarUrl: "6",
+        //     content: "Hello from server !", // default content if missing
+        //     type: 'TEXT',
+        // };
+        const chatData = {
+            messageId: randomUUID(),
+            senderId: 'p1',
+            senderName: 'manox lx',
+            avatarUrl: "2",
+            content: "kiss", // default content if missing
+            type: 'EMOJI',
+        };
+        this.chatHistory.push(chatData);
+        this.room.broadcast("RECEIVE_CHAT", chatData);
+        console.log('add-debug-chat ', chatData);
     }
 }
