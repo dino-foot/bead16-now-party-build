460a89de70681e7856557207788c62c2bd05a96c
Shohanur Rahaman <shohan4556@gmail.com>
2026-04-22 16:35:39 +0600

    Update Global.js

--- rooms/Constants/Global.js
+++ rooms/Constants/Global.js
@@ -13,11 +13,15 @@ export const DEFAULT_COUNTRY = "GLOBAL";
 export const DEFAULT_AVATAR_ID = "1";
 export const DEFAULT_AVATAR_URL = "url";
 export const DEFAULT_ENTRY_FEE = 1000;
-export const DEFAULT_TURN_TIME = 60; // seconds
+export const DEFAULT_TURN_TIME = 60; // 60 seconds
 export const TIME_1_MIN_MS = 60000;
 export const FAST_AUTOPLAY_TIME_MS = 10000; // 10 second timeout for disconnected autoplay
 export const DEFAULT_LAST_STAND_MOVE = 50; // to trigger draw if both players reach this number of moves without a winner
-export const DUMMY_PLAYER_TIME_MS = 10000; // 10 seconds, if only 1 player then trigger dummy multiplayer
+export const DUMMY_PLAYER_TIME_MS = 10000; // 10 seconds only 1 player then trigger dummy multiplayer
 export const VERSION = "1.0.0";
 export const GAME_ID = "bead16_party";
 export const MATCH_COMMISSION = 0.2; // 20% commission on total entry fees
+// room clients and specators limits
+export const MAX_PLAYERS = 2;
+export const MAX_SPECTATORS = 6;
+export const MAX_CLIENTS = MAX_PLAYERS + MAX_SPECTATORS; //? 2 + 8 spectators
